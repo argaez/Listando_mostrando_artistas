bb05ba757447e265ab6da10ffc0ab6fe92ac8200
sebastian argaez <sargaezu@gmail.com>
2019-10-26 19:20:35 -0500

    Controlador y Vista preliminar Artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,74 @@
 class ArtistsController < ApplicationController
 
+    before_action :set_artists, only: %i[show edit update destroy]
+
     def index
     
         @artists = Artist.all
 
+    end
+
+    def show
+
+            
+    end
+
+    def new
+    
+        @artist = Artist.new
+
+    end
+
+    def create
+
+        @artist = Artist.new(product_params)
+        if @artist.save
+            redirect_to artists_path
+        else
+            render :new
+        end
+    
+    end
+
+    def edit
+    
+                
+    end
+
+    def update
+
+        if @artist.update(artist_params)
+            redirect_to artists_params
+        else
+            render :edit
+        end
+    end
+    
+    def destroy
+    
+        if artist.destroy
+        	flash[:success] = "Artist has been successfully deleted"
+		else
+			flash[:error] = "Artist could not be deleted"
+        end
+        
+        redirect_to artists_path
+
+    end
+    
+
+    private
+
+    def artist_params
+
+        params.require(:artist).permit(:name, :image_url)
+    
+    end
+
+    def set_artists
+
+        @artist = Artist.find(params[:id])
+    
     end
 
 end
